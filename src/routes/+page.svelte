6879ff42889c7ab4fc98ab06e852cac53d5b4861
unknown <frosia_2016@mail.ru>
2022-12-14 03:43:16 +0300

    1 коммит задания

---
+++ src/routes/+page.svelte
@@ -0,0 +1,50 @@
+<script>
+    import Table from "./table.svelte";
+    
+    let element = 1;
+
+
+    }
+
+</script>
+
+<div class="menu">
+    <button on:click={() => element+=1}>
+        настройки
+    </button>
+    
+    <button>
+        +сеть
+    </button>
+    <button>
+        +сеть
+    </button>
+    <button>
+        .
+    </button>
+</div>
+
+{#if element%2==0}
+<Table></Table>
+{/if}
+
+<!-- <Table></Table> -->
+
+
+<style>
+    .menu{
+        display: flex;
+        flex-direction: column;
+        width: 300px;
+        border-right: 1px black solid;
+        padding-top: 10px;
+    }
+
+    button {
+        height: 30px;
+        background-color: aquamarine;
+        cursor: pointer;
+    }
+</style>
+
+
---
+++ src/routes/table.svelte
@@ -0,0 +1,66 @@
+<script>
+    export let value = '';
+    let listIp = [
+        {id:1, ip: '123.222.1.2', meaning: 'обнаружен', location: 'неизвестно'}
+    ]
+    function hhd(x) {
+        let www= 'не обнаружен'
+                listIp.map(ipvalue => {
+                    if(x === ipvalue.ip) {
+                        www = "обнаружен"
+                    }
+                })
+            return www
+            }
+
+    const addIp = () => {
+        listIp = listIp.concat([{
+            id: Date.now(),
+            ip: value,
+            meaning: hhd(value),
+            location: 'неизвестно'
+        }]);
+    }
+    
+</script>
+
+
+<div class="block-table">
+    <h1>настройки</h1>
+    <div class="form-table">
+        <input type="text" bind:value>
+        <button class="start-btn" on:click={addIp}>START</button>
+    </div>
+    <table border=".5px" cellspacing=0 cellpadding=10>
+        <tr>
+            <td>IP</td>
+            <td>Значение</td>
+           <td>индекс</td>
+        </tr>
+        {#each listIp as ipp}
+            <tr>
+                <td>{ipp.ip}</td>
+                <td>{ipp.meaning}</td>
+                <td>{ipp.location}</td>
+            </tr>
+        {/each}
+    </table>
+</div>
+
+<style>
+    .block-table {
+        display: block;
+        margin-left: 50%;
+        margin-right: 50%;
+        margin-top: 50px;
+    }
+
+    .form-table {
+        display: flex;
+        margin-bottom: 60px;
+    }
+    .start-btn {
+        margin-left: 50px;
+        background-color: brown;
+    }
+</style>
